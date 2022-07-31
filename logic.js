c016672f4a984261a87d49df9f4aa188604d8652
Enoch <mmejuenoch@gmail.com>
2022-08-01 00:58:28 +0100

    [JS]: Done with the second and final part of the Javascript for the {initial-project}

--- logic.js
+++ logic.js
@@ -58,4 +58,65 @@ function createList() {
 
       draggable_list.appendChild(listItem);
     });
+
+    addEventListeners();
+}
+
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+
+  dragListItems.forEach(item => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
+}
+
+function dragStart() {
+  dragStartIndex = +this.closest("li").getAttribute('data-index');
+}
+function dragEnter() {
+  this.classList.add("over");
+}
+function dragLeave() {
+  this.classList.remove("over");
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove("over");
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+check.addEventListener("click", checkOrder);
+
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector(".draggable").innerText.trim();
+
+    if(personName !== richestPeople[index]) {
+      listItem.classList.add("wrong");
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
+  });
 }
